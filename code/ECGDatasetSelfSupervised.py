007ef6e025179ce6920e70569ba5a113d1bdb211
Edoardo Coppola <59709779+Edoar-do@users.noreply.github.com>
2023-06-15 17:52:15 +0200

    corrected path from which to load ECG matrix in ECGDataset

--- code/ECGDatasetSelfSupervised.py
+++ code/ECGDatasetSelfSupervised.py
@@ -15,26 +15,20 @@ from Configs import get_configs
 class ECGDatasetSelfSupervised(Dataset):
     def __init__(self, path_to_dataset_csv, ecg_dir_path):
         self.ecg_dataframe = pd.read_csv(path_to_dataset_csv, dtype={'filename': str})
-        self.ecg_dir_path = ecg_dir_path
-        self.physio_regex = r'^[A-Z]+\d+'
-        configs = get_configs("./code/configs.json")
-        self.target_fs = configs["target_fs"]
-        self.window = configs["window"]
-        self.n_leads = configs["n_leads"]
-        self.filter_bandwidth = configs["filter_bandwidth"]
-        self.patch_size = configs["patch_size"]
-        self.mask_token = configs["mask_token"]
-        self.mask_perc = configs["mask_perc"]
+        self.ecg_dir_path = ecg_dir_path # something like "./***_self_supevised", *** in {train, val, test}
+
     
     def __len__(self):
         return len(self.ecg_dataframe)
     
     def __getitem__(self, idx):
         record = self.ecg_dataframe.iloc[idx]
-        ecg_filename = record['filename'] #something like "./***_self_supevised", *** in {train, val, test}
+        ecg_filename = record['filename'] # the mere filename
         age = np.nan
         sex = np.nan
 
-        ecg_data = np.load(ecg_filename) #load a pre-processed 12 x 5000 ndarray
+        ecg_path = os.path.join(self.ecg_dir_path, ecg_filename)
+
+        ecg_data = np.load(ecg_path) #load a pre-processed 12 x 5000 ndarray
 
         return torch.from_numpy(ecg_data), age, sex
